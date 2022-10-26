4c0cc749e09e1adfbc794bbf432b2f00bf11f54d
Luke <saturnhafen@gmail.com>
2022-10-26 13:52:00 +0200

    Fixes #3

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  it "should remember the values that the author was initialzed with" do
+    @author = Author.new({:first_name => 'Allan', :last_name => 'Turing', :homepage => 'http://wikipedia.org/Alan_Turing'})
+
+    expect(@author.first_name).to eq('Allan')
+    expect(@author.last_name).to eq('Turing')
+    expect(@author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+    
+    expect(@author.name).to eq('Allan Turing')
+  end
+end
