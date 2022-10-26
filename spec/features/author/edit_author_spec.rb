4bb655eb5956c5ea1502e8cfc0844e891d8fc5d6
Luke <saturnhafen@gmail.com>
2022-10-26 15:59:20 +0200

    Fixes #8

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
-describe "New author page", type: :feature do
-  it "should exist at 'new_author_path' and render withour error" do
+describe "Edit author page", type: :feature do
+  it "should exist at 'edit_author_path' and render withour error" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
-    visit new_author_path
+    @alan = FactoryBot.create :author
+
+    visit edit_author_path(@alan)
   end
   
   it "should have text inputs for an author's first name, last name, and homepage" do
-    visit new_author_path
+    @alan = FactoryBot.create :author
+
+    visit edit_author_path(@alan)
     # these are the standard names given to inputs by the Rails form builder
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
@@ -15,15 +19,18 @@ describe "New author page", type: :feature do
   end
 
   it "should show validation error" do
-    visit new_author_path
+    @alan = FactoryBot.create :author
+
+    visit edit_author_path(@alan)
 
     expect(page).to_not have_text('1 error prohibited this article from being saved:')
     expect(page).to_not have_text('Last name can\'t be blank')
 
     fill_in 'author[first_name]', with: 'Alan'
+    fill_in 'author[last_name]', with: ''
     fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
 
-    click_button 'Save Author'
+    click_button 'Update Author'
 
     expect(page).to have_text('1 error prohibited this article from being saved:')
     expect(page).to have_text('Last name can\'t be blank')
