96c839d018b015daf2cab0b183335b888c24b98d
Luke <saturnhafen@gmail.com>
2022-10-26 21:12:51 +0200

    Fixes #17

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,15 @@
 class Author < ApplicationRecord
+    validates :last_name, presence: true
 
     def name
         first_name + " " + last_name
     end
 
-    validates :last_name, presence: true
+    def papers=(papers)
+        @papers = papers
+    end
+
+    def papers
+        @papers.to_a
+    end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -16,4 +16,10 @@ RSpec.describe Author, type: :model do
 
     expect(@author).to_not be_valid
   end
+
+  it "should have a list of papers" do
+    @author = Author.new({:first_name => 'Alan', :last_name => 'Turing', :homepage => 'http://wikipedia.org/Alan_Turing'})
+
+    expect(@author.papers).to be_kind_of Array
+  end
 end
